06c0fb1059d85ab411bbebce285df5711efd2496
Elaina Midcap <emidcap24@biomedscienceacademy.org>
2020-11-10 15:32:53 +0000

    I changed the f string and added a few comments

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Elaina Midcap 
+Period/Core: 5 
+
 
 
 """
+#this will print the f/string and the intro
+print(f"Hello,"+ "user")
+print("I hope you have a\nGood day")
+
+#the next series of input's will ask for the answers for the madlib 
+x=input ("Enter a name:")
+
+y=input ("Enter a location:")
+
+z = input("Enter a food:")
+
+a = input("Enter a drink:") 
+
+b = input("Enter a building:")
+
+j= input ("Enter a place to stay in:")
+
+c = input("Enter a color:")
+
+d =input("Enter a type of car:")
+
+f= input ("Enter a past-tense verb:")
+
+g= input("Enter a luxury item:")
+
+#this will print the madlib 
+print(x, "decided to travel to",y)
+print("The first thing",x, "did was eat a",z)
+print("and drank a",a,"with it")
+print(x, "then needed to go inside a",b) 
+print("After",x,"had left they needed to go to a",j)
+print("To get to the",j,x,"got into a",c,d)
+print(x,f,"to their room")
+print(x,"ordered a",g,"and enjoyed \n the rest of their night" )
